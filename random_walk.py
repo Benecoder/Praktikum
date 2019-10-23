5ec95ffb9b52e619d68ca76f0adabcb04d7aed50
Benedikt.Heyl <benedikt.heyl@physik.uni-muenchen.de>
2019-10-23 18:27:16 +0200

    D calculation

--- random_walk.py
+++ random_walk.py
@@ -1,30 +1,54 @@
 #! /usr/bin/env python3
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.rcParams['text.usetex'] = True
 
 
-no_steps = 10000
-coords = np.zeros((no_steps,3))
+np.random.seed(1)
 
-def get_step(coord):
-    step = np.random.rand(3)-0.5
-    step /= np.linalg.norm(step)
-    return coord+step
-
-for t in range(1,no_steps):
-    coords[t] = get_step(coords[t-1])
-
-r_2 = np.linalg.norm(coords,axis=1)**2
-D = np.mean(r_2)/(6*no_steps)
-D_array = np.cumsum(r_2)/(6*(np.arange(1,1+no_steps)**2))
+no_steps = 1000
+steps = (np.random.rand(no_steps,3)-0.49)
+coords = np.cumsum(steps,axis=0)
 
 
-print(D)
-print(D_array[-1])
+def plot_D(steps):
 
-plt.plot(D_array)
-#plt.plot(coords[:,0],coords[:,1])
-plt.show()
+    result = np.zeros(no_steps)
+
+    for delta_t in range(1,no_steps):
+        r_2_buffer = np.empty(no_steps-delta_t)
+        for start_index in range(no_steps-delta_t):
+            path = np.cumsum(steps[start_index:start_index+delta_t],axis=0)
+            r_2_buffer[start_index] = np.sum(path[-1]**2)
+        result[delta_t] = np.mean(r_2_buffer)
+
+    delta_t = np.arange(no_steps)
+    fit_limit = 550
+
+    lin_coeff = np.polyfit(delta_t[:fit_limit],result[:fit_limit],1)
+    quad_coeff = np.polyfit(delta_t[:fit_limit],result[:fit_limit],2)
+    lin_fit = np.poly1d(lin_coeff)
+    quad_fit = np.poly1d(quad_coeff)
+    lin_graph = lin_fit(delta_t[:fit_limit])
+    quad_graph = quad_fit(delta_t[:fit_limit])
+
+    print('linear coefficents: '+str(lin_coeff))
+    print('quad coefficents: '+str(quad_coeff))
+
+    plt.xlabel('$\Delta t$',fontsize=15)
+    plt.ylabel('$<r^2> over \Delta t$',fontsize=15)
+    plt.plot(lin_graph,'--',color = 'green')
+    plt.plot(quad_graph,'--',color = 'red')
+    plt.plot(result,color = 'blue')
+    plt.plot([fit_limit,fit_limit],[0,np.max(result)],color = 'gray',alpha=0.5)
+    plt.show()
+
+def plot_2d():
+    plt.plot(coords[:,0],coords[:,1])
+    plt.show()
 
 
+if __name__ == '__main__':
+    plot_2d()
